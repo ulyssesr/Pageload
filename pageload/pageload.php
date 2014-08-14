03f202838bf53bf636d10a97e011eaebdba82dc7
Ulysses <ulyssesr@yahoo.com>
2014-08-14 14:52:47 -0700

    changed to lower priority. updated info.

--- pageload/pageload.php
+++ pageload/pageload.php
@@ -1,17 +1,20 @@
 <?php
 /*
+
 Plugin Name: Page Load Time
-Plugin URI: http://ulyssesonline.com
-Description: Displays page load times in footer.
-Version: 0.1
+Plugin URI: http://uly.me
+Description: Displays page load times in the footer of the theme.
+Version: 0.2
 Author: Ulysses Ronquillo
-Author URI: http://ulyssesonline.com
+Author URI: http://uly.me
+
 */
 
-function ur_pageload_footer() {
+function urr_pageload_footer() {
   echo 'Page in '; timer_stop(1); echo ' seconds.';  
 } 
 
-add_action('wp_footer', 'ur_pageload_footer');
+add_action('wp_footer', 'urr_pageload_footer', 100);
 
-?>
+
+/* end of file */
